d93ef8771623238d481ce1c4c384c6e79c63b350
OmniControll <qais13@hotmail.com>
2023-10-05 19:24:43 +0200

    changed model to log scale, evaluated the model a little bit

--- graph_stx.py
+++ graph_stx.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from pandas.plotting import register_matplotlib_converters
 from statsmodels.tsa.seasonal import seasonal_decompose
+import numpy as np
+import seaborn as sns
+
 
 register_matplotlib_converters()
 
@@ -14,75 +17,90 @@ tickerSymbol = 'BTC-USD'
 tickerData = yf.Ticker(tickerSymbol)
 
 # Get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2020-1-1', end='2023-9-30')
+tickerDf = tickerData.history(period='1d', start='2016-1-1', end='2023-9-30')
+
+# Drop Dividends and Stock Splits because we're dealing with a cryptocurrency
+tickerDf = tickerDf.drop(['Dividends', 'Stock Splits'], axis=1)
+
+#transform target variable (log closing price), to logaritmic scale because we're doign time series analysis
+tickerDf['Log_Close'] = np.log(tickerDf['Close'])
+
+print(tickerDf.describe())
+
+#EDA
 
 # Plot the close prices
 plt.figure(figsize=(10, 8))
-plt.plot(tickerDf.Close)
-plt.title('Closing price: '+tickerSymbol)
+sns.lineplot(x = 'Date', y= 'Close', data = tickerDf)
+plt.title('Log of Closing Price: '+tickerSymbol)
 plt.xlabel('Date')
-plt.ylabel('Closing Price ($)')
+plt.ylabel('Log of Closing Price')
 plt.grid(True)
 plt.show()
+
 
 # Moving Average Crossover
-short_rolling = tickerDf.Close.rolling(window=20).mean()
-long_rolling = tickerDf.Close.rolling(window=100).mean()
+short_rolling = tickerDf['Log_Close'].rolling(window=20).mean()
+long_rolling = tickerDf['Log_Close'].rolling(window=100).mean()
 plt.figure(figsize=(10,8))
-plt.title('Moving Average Crossover: '+tickerSymbol)
-plt.plot(short_rolling, label='20 days rolling')
-plt.plot(long_rolling, label='100 days rolling')
+sns.lineplot(data = short_rolling, label='20-day rolling mean')
+sns.lineplot(data = long_rolling, label='100-day rolling mean')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+# Exponential Moving Average Crossover
+ema_short = tickerDf['Log_Close'].ewm(span=20, adjust=False).mean()
+ema_long = tickerDf['Log_Close'].ewm(span=100, adjust=False).mean()
+plt.figure(figsize=(10,8))
+sns.lineplot(data = ema_short, label='20-day EMA')
+sns.lineplot(data = ema_long, label='100-day EMA')
+plt.legend()
+plt.grid(True)
+plt.show()
+
 
 # Linear Regression
-df = tickerDf.reset_index()
-df['timestamp'] = df['Date'].apply(lambda x: x.timestamp())
-X = df['timestamp'].values.reshape(-1, 1)
-Y = df['Close'].values
-model = LinearRegression()
-model.fit(X, Y)
-trend = model.predict(X)
+tickerDf = tickerDf.reset_index()
+tickerDf['timestamp'] = tickerDf['Date'].apply(lambda x: x.timestamp())
+X = tickerDf['timestamp'].values.reshape(-1, 1)
+Y = tickerDf['Log_Close'].values
+log_model = LinearRegression()
+# train the beast
+log_model.fit(X, Y)
+log_trend = log_model.predict(X)
+
+# Plot the beast 
 plt.figure(figsize=(10,8))
 plt.title('Linear Regression: '+tickerSymbol)
-plt.plot(df['Date'], tickerDf.Close, label='Actual Close Price')
-plt.plot(df['Date'], trend, label='Predicted Close Price', color='r')
+sns.lineplot(x=tickerDf['Date'], y=tickerDf['Log_Close'], label='Log Closing Price')
+sns.lineplot(x=tickerDf['Date'], y=log_trend, label='Predicted Log Closing Price')
+plt.xlabel('Date')
+plt.ylabel('Log Closing Price')
 plt.legend()
 plt.grid(True)
 plt.show()
 
+# the line looks to be quite underfit. This is because the linear regression model is not able to capture the seasonality of the data.
 
-print(df.describe())
+#  time-series decomposition: to decompose the time series into its components: seasonality, trends, and residuals
 
-# Drop Dividends and Stock Splits
-df = df.drop(['Dividends', 'Stock Splits'], axis=1)
-
-print(df.info())
-
-# Examine the BTC price's seasonality, trends, and residuals using time-series decomposition
-result = seasonal_decompose(df['Close'], model='multiplicative', period=30)
+result = seasonal_decompose(tickerDf['Log_Close'], model='multiplicative', period=30)
 fig = plt.figure()
 fig = result.plot()
 fig.set_size_inches(10, 8)
 plt.show()
+#Residual is the difference between the observed value and the predicted value
+#The residual is the part of the data that the model is unable to explain
+# th residual is near 1, this means the decomposition may have left out some structure in the data, which makes sense because our line does not fit the data perfectly
+
 
 # Examine the BTC price's seasonality, trends, and residuals using time-series decomposition with an additive model instead of multiplicative
 #to compare the results
-result = seasonal_decompose(df['Close'], model='additive', period=30)
+result = seasonal_decompose(tickerDf['Close'], model='additive', period=30)
 fig = plt.figure()
 fig = result.plot()
 fig.set_size_inches(10, 8)
 plt.show()
 
-
-# Check if the data has a daily or weekly pattern.
-
-# Daily
-df['day'] = df['Date'].dt.dayofweek
-df.groupby('day')['Close'].mean().plot(kind='bar')
-plt.title('Daily Pattern')
-plt.xlabel('Day of Week')
-plt.ylabel('Average Close Price')
-plt.grid(True)
-plt.show()
+# Examine the BT
